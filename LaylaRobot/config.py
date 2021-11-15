f26e039555a4931fb7b48fbf5f29d1fcc119122f
TheKmax <90910711+TheKmax@users.noreply.github.com>
2021-11-15 13:57:20 +0530

    Update config.py

--- LaylaRobot/config.py
+++ LaylaRobot/config.py
@@ -14,28 +14,28 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
+    API_ID =  3898519 # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 797768146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "HEROGAMERS1" 
-    SUPPORT_CHAT = "AwesomeSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1352499461 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "ihaveenoughhate"
+    SUPPORT_CHAT = "foundingtitansupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001392103597
+        -1001306188318
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001392103597
+        -1001445585494
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules # its "URI" and not "URL" as herok and similar ones only accept it as such
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "SpamWatchSupport"
+    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
@@ -67,6 +67,7 @@ class Config(object):
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
+    
 
 class Production(Config):
     LOGGER = True
@@ -74,3 +75,5 @@ class Production(Config):
 
 class Development(Config):
     LOGGER = True
+
+    ALLOW_CHATS = True
